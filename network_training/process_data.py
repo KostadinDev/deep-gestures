349dabaaff9d3184c9281b2e2f34b05102c6668b
Daniel Bacal <daniel.bacal@stonybrook.edu>
2021-05-10 12:20:00 -0400

    Implemented data augmentation and built upon CNN and data processing.

--- network_training/process_data.py
+++ network_training/process_data.py
@@ -6,9 +6,10 @@ import pandas as pd
 import numpy as np
 import csv
 from scipy.interpolate import interp1d
-from mpl_toolkits import mplot3d
+# from mpl_toolkits import mplot3d
 import sys
 import matplotlib.pyplot as plt
+from data_augmentation import data_augment
 
 
 # Takes in a gesture entry and interpolates 128 data points
@@ -27,21 +28,25 @@ def interpolate(entry):
     return interpolated_entry
 
 
-def format_data(file_path, label):
+def format_data(file_path, label, data_format):
     data = []
-    size = 0
+    # size = 0
     with open(file_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         entry = []
         for row in csv_reader:
-            row[0] = row[0][16:]
+            if data_format == 'Category':
+                row[0] = row[0][16:]
             if row[0] == 'EXIT':
                 try:
                     entry = interpolate(np.array(entry))
                     data.append(np.array([entry, label]))
+                    # Apply data augmentation from the current entry
+                    for _ in range(1000):
+                        data.append(np.array([data_augment(entry), label]))
                 except:
-                    print('')
-                size += 1
+                    pass
+                # size += 1
             elif row[0] == 'BEGIN':
                 entry = []
             else:
@@ -54,7 +59,12 @@ if __name__ == '__main__':
     input_path = sys.argv[1]
     output_path = sys.argv[2]
     label = sys.argv[3]
-    data = format_data(input_path, label)
+    data_format = ''
+    try:
+        data_format = sys.argv[4]
+    except:
+        data_format = 'Category'
+    data = format_data(input_path, label, data_format)
     np.save(output_path, data, allow_pickle=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
