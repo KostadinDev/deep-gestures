53764b30d731aa13820fd4b2a022e4e54c440111
Jerry Thomas <jerry.l.thomas@stonybrook.edu>
2021-05-10 15:45:46 -0400

    Added comments to train.py

--- network_training/train.py
+++ network_training/train.py
@@ -30,19 +30,7 @@ if __name__ == '__main__':
     # Get X data and format into training format
     X = data[:, 0]
     X = np.vstack(X).astype('float32')
-    # print("Standard deviation")
-    # np.std(X)
-    # print("MEAN: ")
-    # np.mean(X)
-    # # Normalize the data
-    # print("Before:")
-    # print(X)
-    # keras.utils.normalize(
-    #     X, axis=-1, order=2
-    # )
-    # print("After")
-    # print(X)
-
+    
     X = np.reshape(X, (int(len(X) / 128), 128, 3, -1))
     
 
@@ -52,6 +40,8 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
+    # Custom Metric Functions
+    
     def recall_m(y_true, y_pred):
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -86,12 +76,14 @@ if __name__ == '__main__':
     # Set up hyper parameters
     optimizer = keras.optimizers.Adam(learning_rate=0.001)
     model.compile(optimizer=optimizer, loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-                #   metrics=['accuracy', f1_m, precision_m, recall_m])
+    
+    # Add the line below if you are interested in all metrics
+    #   metrics=['accuracy', f1_m, precision_m, recall_m])
 
     # Start training
     history = model.fit(X_train, y_train, epochs=20, validation_data=(X_val, y_val))
     # Evaluate
-    # test_loss, test_acc, test_f1, test_prec, test_recall = model.evaluate(X_test, y_test, verbose=2)
+    
     test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
     training_loss = history.history['loss']
     test_loss = history.history['val_loss']
@@ -102,6 +94,8 @@ if __name__ == '__main__':
     # Create count of the number of epochs
     epoch_count = range(1, len(training_loss) + 1)
     
+    # Saving the kera model 
+
     model.save('lite_models/model_keras.h5')
     # Visualize loss history
     plt.plot(epoch_count, training_loss, 'r--')
@@ -110,7 +104,7 @@ if __name__ == '__main__':
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.show()
-
+    
     epoch_count = range(1, len(training_accuracy) + 1)
     plt.plot(epoch_count, training_accuracy, 'r--')
     plt.plot(epoch_count, test_accuracy, 'b-')
